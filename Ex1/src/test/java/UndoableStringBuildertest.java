250403586e6587464204e6a3b3970fdd91a5ef71
noyrosenbaum <noyrosen123@gmail.com>
2022-12-30 12:30:45 +0200

    Import USB code to USB test file

--- Ex1/src/test/java/UndoableStringBuildertest.java
+++ Ex1/src/test/java/UndoableStringBuildertest.java
@@ -1,9 +1,10 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import observer.UndoableStringBuilder;
 
 import org.junit.jupiter.api.Test;
 
-public class TestUndoableStringBuilder {
+public class UndoableStringBuildertest {
 
   @Test
   public void testEMPConstructor() {

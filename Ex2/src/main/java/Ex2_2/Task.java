eb57e59307967a031db4836a8ac856fdb1cf7b76
noyrosenbaum <noyrosen123@gmail.com>
2023-01-10 18:00:28 +0200

    Modify Task to be with no errors

--- Ex2/src/main/java/Ex2_2/Task.java
+++ Ex2/src/main/java/Ex2_2/Task.java
@@ -2,16 +2,15 @@ package Ex2_2;
 
 import java.util.concurrent.*;
 
-//createtask gets a lambda experession and a task type
+//createtask gets a lambda experession - callable and a task type
 //we get the string next to the number in the enum
 public class Task<T> implements Callable<T>, Comparable<Task<T>> {
 
     private T type;
     private Callable<T> task;
-    private int typePriority;
     private TaskType taskType;
 
-//    private static Comparator<Task<T>> comparator = new Task<T>()Comparator();
+    //    private static Comparator<Task<T>> comparator = new Task<T>()Comparator();
 
     //constructor
     private Task(Callable<T> task, TaskType taskType) {
@@ -20,30 +19,20 @@ public class Task<T> implements Callable<T>, Comparable<Task<T>> {
         this.task = task;
     }
 
-    public Callable<T> getTask() {
-        return this.task;
-    }
-
-    public int getPriority() {
-        return taskType.getPriorityValue();
-    }
-
-    public void setTask(Callable<T> task) {
-        this.task = task;
-    }
-
-    public void setPriority() {
-        this.taskType.setPriority(typePriority);
-    }
-
-    @Override
-    public int compareTo(Task<T> otherTask) {
-        return Integer.compareTo(otherTask.taskType.getPriorityValue(), this.taskType.getPriorityValue());
-    }
-
-    //factory method
+    //factory method with declared task typed
     public static <T> Task<T> createTask(Callable<T> task, TaskType taskType) {
         return new Task<T>(task, taskType);
+    }
+
+    //factory method with default task type - OTHER
+    public static <T> Task<T> createTask(Callable<T> task) {
+        return new Task<T>(task, TaskType.OTHER);
+    }
+
+    //comparison
+    @Override
+    public int compareTo(Task<T> otherTask) {
+        return Integer.compare(otherTask.taskType.getPriorityValue(), this.taskType.getPriorityValue());
     }
 
     @Override
@@ -55,5 +44,23 @@ public class Task<T> implements Callable<T>, Comparable<Task<T>> {
             throw new RuntimeException(e);
         }
         return null;
+    }
+
+    public Callable<T> getTask() {
+        return this.task;
+    }
+
+    public int getPriority() {
+        return taskType.getPriorityValue();
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "type=" + type +
+                ", task=" + getTask() +
+                ", typePriority=" + getPriority() +
+                ", taskType=" + taskType +
+                '}';
     }
 }
